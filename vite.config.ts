0df139fdbc10fb4b8bc29f9f92f6c5c1cb57efb7
Jesse Baker <jcb.121@hotmail.co.uk>
2025-10-28 20:56:35 +0100

    Fix: Firebase build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        fixtures: resolve(__dirname, "fixtures.html"),
+        index: resolve(__dirname, "index.html"),
+        main: resolve(__dirname, "main.html"),
+        scene: resolve(__dirname, "scene.html"),
+        venue: resolve(__dirname, "venue.html"),
+        venues: resolve(__dirname, "venues.html"),
+      },
+    },
+  },
   plugins: [react()],
-})
+});
