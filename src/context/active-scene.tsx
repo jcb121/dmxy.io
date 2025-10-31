5e8fc27dacde5d9e921b66638325646cc3206c55
Jesse Baker <jcb.121@hotmail.co.uk>
2025-10-31 10:19:45 +0100

    Feat: Allow persisting of current scene for page reloads

--- src/context/active-scene.tsx
+++ src/context/active-scene.tsx
@@ -1,18 +1,26 @@
 import { create } from "zustand";
 import { Scene } from "./scenes";
+import { persist } from "zustand/middleware";
 
 export const useActiveScene = create<{
   activeScenes: Scene[];
   setActiveScenes: (scenes: Scene[]) => void;
-}>((set) => {
-  return {
-    activeScenes: [],
-    setActiveScenes: (activeScenes: Scene[]) =>
-      set((state) => {
-        return {
-          ...state,
-          activeScenes: activeScenes.map((s) => ({ ...s })),
-        };
-      }),
-  };
-});
+}>()(
+  persist(
+    (set) => {
+      return {
+        activeScenes: [],
+        setActiveScenes: (activeScenes: Scene[]) =>
+          set((state) => {
+            return {
+              ...state,
+              activeScenes: activeScenes.map((s) => ({ ...s })),
+            };
+          }),
+      };
+    },
+    {
+      name: "active-scene",
+    }
+  )
+);
